d86172e8b92b29c2ad92ca6402642aea046e0032
JohnserfSeed <johnserf-seed@foxmail.com>
2023-09-04 00:22:30 +0800

    🥙重写路由 #25 #22 #21 #17 #14 #5 #3
    1、让路由文件更加简洁
    2、错误处理中间件

--- routes/api.js
+++ routes/api.js
@@ -15,6 +15,12 @@ router.get('/', async function(req, res, next) {
             dyCookie = null;
         }
 
+        // 如果dycookie不存在或无法解析rs
+        const allEmpty = Object.values(dyCookie).every(value => value === '');
+        if (allEmpty) {
+            res.render('index', { videoData: {work:false} });
+            return;
+        }
 
         // 如果未提供URL，则使用默认视频
         let videoUrl = req.query.url;
@@ -22,6 +28,13 @@ router.get('/', async function(req, res, next) {
             videoUrl = 'https://v.douyin.com/NKyY6Ch/';
         }
 
+        // 检查URL的有效性（简单的）
+        if (!videoUrl.startsWith('https://v.douyin.com/') && !videoUrl.startsWith('https://www.douyin.com/')) {
+            throw new Error('无效的URL地址');
+        }
+
+        // 根据提供的URL获取视频ID
+        const videoId = await GetID(videoUrl);
 
         // 使用视频ID和cookie获取视频详情
         const videoData = await GetInfo(videoId, dyCookie, getXB);
@@ -31,6 +44,17 @@ router.get('/', async function(req, res, next) {
     }
 });
 
+// 错误处理中间件
+router.use((err, req, res, next) => {
+    console.error(err.stack)
+    // 不要尝试嗅探并覆盖响应的MIME类型
+    res.setHeader('X-Content-Type-Options', 'nosniff');
+    // 根据错误类型或消息为用户提供不同的反馈
+    if (err.message.includes('无效的URL地址')) {
+        res.status(400).render('error', { message: '提供的URL无效' });
+    } else {
+        res.status(500).render('error', { message: '服务器内部错误' });
+    }
 });
 
 module.exports = router;
